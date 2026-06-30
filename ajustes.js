1c1fc77667a7b81cb1be89f93d7e35c9a97472f6
Lusibel <91235367+Lusibel@users.noreply.github.com>
2026-06-29 23:57:44 -0600

    Update ajustes settings and add new functions

--- ajustes.js
+++ ajustes.js
@@ -28,11 +28,13 @@ if (ajustes.vision > 0.2) {
 
 const ajustes = {
 
-    vision: 0.5,
-    dialogScale: 1,
-    joystickScale: 1,
+    vision: 2,
+    dialogScale: 0.5,
+    joystickScale: 0.5,
+    joystickOpacity: 0.5,
+    hudScale: 0.5,
     musicVolume: 0.5,
-    sfxVolume: 1,
+    sfxVolume: 0.5,
 
 };
 
@@ -87,23 +89,173 @@ function aplicarVision() {
 
 }
 
+
 function aplicarEscalaDialogo() {
 
-}
+    document.getElementById("dialogoValor").textContent =
+        ajustes.dialogScale.toFixed(1);
 
+    const dialogo = document.getElementById("dialogBox");
+
+    dialogo.style.transformOrigin = "bottom center";
+
+    dialogo.style.transform =
+        `translateX(-50%) scale(${ajustes.dialogScale})`;
+
+}
 function aplicarEscalaJoystick() {
 
+    document.getElementById("joystickValor").textContent =
+        ajustes.joystickScale.toFixed(1);
+
+    document.getElementById("joystick").style.transform =
+        `scale(${ajustes.joystickScale})`;
+
 }
+
+function aplicarOpacidadJoystick() {
+
+    document.getElementById("opacidadValor").textContent =
+        ajustes.joystickOpacity.toFixed(1);
+
+    document.getElementById("joystick").style.opacity =
+        ajustes.joystickOpacity;
+
+}
+
+function aplicarEscalaHUD() {
+
+    document.getElementById("hudValor").textContent =
+        ajustes.hudScale.toFixed(1);
+
+    document.getElementById("playerLives").style.transform =
+        `scale(${ajustes.hudScale})`;
+
+}
+
 
 function aplicarVolumen() {
 
 }
 
 cargarAjustes();
+
 aplicarVision();
 aplicarEscalaDialogo();
 aplicarEscalaJoystick();
+aplicarOpacidadJoystick();
+aplicarEscalaHUD();
 aplicarVolumen();
+
+document.getElementById("joystickMas").onclick = () => {
+
+    if (ajustes.joystickScale < 2.0) {
+
+        ajustes.joystickScale += 0.1;
+
+    }
+
+    guardarAjustes();
+    aplicarEscalaJoystick();
+
+};
+
+document.getElementById("joystickMenos").onclick = () => {
+
+    if (ajustes.joystickScale > 0.5) {
+
+        ajustes.joystickScale -= 0.1;
+
+    }
+
+    guardarAjustes();
+    aplicarEscalaJoystick();
+
+};
+
+document.getElementById("opacidadMas").onclick = () => {
+
+    if (ajustes.joystickOpacity < 1.0) {
+
+        ajustes.joystickOpacity =
+    Number((ajustes.joystickOpacity + 0.1).toFixed(1));
+
+    }
+
+    guardarAjustes();
+    aplicarOpacidadJoystick();
+
+};
+
+document.getElementById("opacidadMenos").onclick = () => {
+
+    if (ajustes.joystickOpacity > 0.2) {
+
+        ajustes.joystickOpacity =
+    Number((ajustes.joystickOpacity - 0.1).toFixed(1));
+
+    }
+
+    guardarAjustes();
+    aplicarOpacidadJoystick();
+
+};
+
+document.getElementById("dialogoMas").onclick = () => {
+
+    if (ajustes.dialogScale < 2.0) {
+
+        ajustes.dialogScale =
+            Number((ajustes.dialogScale + 0.1).toFixed(1));
+
+    }
+
+    guardarAjustes();
+    aplicarEscalaDialogo();
+
+};
+
+document.getElementById("dialogoMenos").onclick = () => {
+
+    if (ajustes.dialogScale > 0.5) {
+
+        ajustes.dialogScale =
+            Number((ajustes.dialogScale - 0.1).toFixed(1));
+
+    }
+
+    guardarAjustes();
+    aplicarEscalaDialogo();
+
+};
+
+document.getElementById("hudMas").onclick = () => {
+
+    if (ajustes.hudScale < 2.0) {
+
+        ajustes.hudScale =
+            Number((ajustes.hudScale + 0.1).toFixed(1));
+
+    }
+
+    guardarAjustes();
+    aplicarEscalaHUD();
+
+};
+
+document.getElementById("hudMenos").onclick = () => {
+
+    if (ajustes.hudScale > 0.5) {
+
+        ajustes.hudScale =
+            Number((ajustes.hudScale - 0.1).toFixed(1));
+
+    }
+
+    guardarAjustes();
+    aplicarEscalaHUD();
+
+};
 
 document.getElementById("closeSettings")
 .addEventListener("click", cerrarAjustes);
